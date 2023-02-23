9270d60e3a6116ee6c3181de8897c054c6edebbe
Andy Byers <a.byers@bbk.ac.uk>
2023-02-23 16:16:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='janeway-ftp',
-    version='0.3',
+    version='0.4',
     author='OLH Tech',
     author_email='olh-tech@bbk.ac.uk',
     packages=['janeway_ftp'],
